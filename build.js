c096937ce7086b263383c3761ab0a840979f6937
Debbl <me@aiwan.run>
2023-04-04 14:24:33 +0800

    feat: node-html-parser

--- build.js
+++ build.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const fs = require("fs");
 const ejs = require("ejs");
+const { parse } = require("node-html-parser");
 
 const pagesPath = path.resolve(__dirname, "./src/pages");
 const distPath = path.resolve(__dirname, "dist");
@@ -11,7 +12,17 @@ function init() {
   copyDir(pagesPath, distPath);
   // generate index.html
   const projectNames = fs.readdirSync(pagesPath);
-  ejs.renderFile(indexPath, { names: projectNames }, {}, (err, str) => {
+  const projectsInfo = projectNames.map((p) => {
+    const root = parse(
+      fs.readFileSync(path.join(pagesPath, p, "index.html")).toString("utf-8")
+    );
+    return {
+      path: p,
+      title: root.querySelector("title").textContent,
+    };
+  });
+
+  ejs.renderFile(indexPath, { projectsInfo }, {}, (err, str) => {
     fs.writeFileSync(path.join(distPath, "index.html"), str);
   });
 }
