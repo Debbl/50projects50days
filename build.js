4d46af22a87167acb8cf68e4227df8c43f420fa4
Debbl <me@aiwan.run>
2023-04-11 12:48:56 +0800

    chore: build

--- build.js
+++ build.js
@@ -3,6 +3,7 @@ const fs = require("fs");
 const ejs = require("ejs");
 const { parse } = require("node-html-parser");
 const { minify } = require("html-minifier-terser");
+const { rimrafSync } = require("rimraf");
 
 const pagesPath = path.resolve(__dirname, "./src/pages");
 const distPath = path.resolve(__dirname, "dist");
@@ -10,6 +11,8 @@ const publicPath = path.relative(__dirname, "public");
 const indexPath = path.resolve(publicPath, "index.ejs");
 
 function init() {
+  // delete dist
+  rimrafSync(distPath);
   // write
   copyDir(pagesPath, distPath);
   copyDir(publicPath, distPath);
@@ -26,12 +29,17 @@ function init() {
   });
 
   ejs.renderFile(indexPath, { projectsInfo }, {}, (err, str) => {
-    minify(str, { collapseWhitespace: true, minifyCSS: true, minifyJS: true }).then((s) => {
+    minify(str, {
+      collapseWhitespace: true,
+      minifyCSS: true,
+      minifyJS: true,
+    }).then((s) => {
       fs.writeFileSync(path.join(distPath, "index.html"), s);
     });
   });
 }
 
+// utils
 function copy(src, dest) {
   const stat = fs.statSync(src);
   if (stat.isDirectory()) {
